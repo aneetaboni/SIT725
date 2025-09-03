8a53d6233524d39119f12d321812dc53c1c109b0
aneetaboni <147371986+aneetaboni@users.noreply.github.com>
2025-09-03 23:06:12 +1000

    Delete wwww directory

--- wwww/online-library/controllers/index.js
+++
@@ -1,1 +0,0 @@
-module.exports = { booksController: require('./booksController') };
--- wwww/online-library/routes/books.js
+++
@@ -1,7 +0,0 @@
-const router = require('express').Router();
-const book = require('../controllers/bookController');
-
-router.get('/', book.list);
-router.post('/', book.create);
-
-module.exports = router;
