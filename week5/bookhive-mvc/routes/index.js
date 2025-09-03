e8cc1882b3948fe110cd3470def4207b64134f84
aneetaboni <147371986+aneetaboni@users.noreply.github.com>
2025-09-03 23:05:33 +1000

    Delete week5 directory

--- week5/bookhive-mvc/routes/index.js
+++
@@ -1,9 +0,0 @@
-// routes/index.js
-const express = require('express');
-const router = express.Router();
-
-router.get('/', (_req, res) => {
-  res.send('BookHive home'); // keep simple for now (no res.render needed)
-});
-
-module.exports = router;
