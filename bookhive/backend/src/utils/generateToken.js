ee44b79bada4003ff4689cf6c1d6afced6c896f9
aneetaboni <147371986+aneetaboni@users.noreply.github.com>
2025-09-03 23:05:59 +1000

    Delete bookhive directory

--- bookhive/backend/src/utils/generateToken.js
+++
@@ -1,6 +0,0 @@
-import jwt from "jsonwebtoken";
-
-export default function generateToken(id, role = "student") {
-  const secret = process.env.JWT_SECRET;
-  return jwt.sign({ id, role }, secret, { expiresIn: "7d" });
-}
