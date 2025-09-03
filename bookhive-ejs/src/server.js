8aff31e42c67f9a7e89007507373de6dc879c4a1
aneetaboni <147371986+aneetaboni@users.noreply.github.com>
2025-09-03 23:05:46 +1000

    Delete bookhive-ejs directory

--- bookhive-ejs/src/server.js
+++
@@ -1,9 +0,0 @@
-import dotenv from "dotenv";
-dotenv.config();
-import http from "http";
-import app from "./app.js";
-
-const PORT = process.env.PORT || 5000;
-http.createServer(app).listen(PORT, () => {
-  console.log(`BookHive (EJS) listening on http://localhost:${PORT}`);
-});
